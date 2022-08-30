51557f6c6d35f021f495b5ee4d0780800b73e797
rige11 <rigell20@ya.ru>
2022-08-30 23:04:40 +0300

    Update broshurator_0.1.py

--- broshurator_0.1.py
+++ broshurator_0.1.py
@@ -15,14 +15,15 @@ log_moves = {}
 if not os.path.isdir('.\\' + directory):
     os.mkdir('.\\' + directory)
 for root, dirs, files in os.walk('.'):
+    [dirs.remove(d) for d in list(dirs) if d == directory]  # remove destination folder from waLK
     for file in files:
-        if re.search(r'.+[.](jpg|png|jpeg|gif)', file.lower()) and os.stat(root + '\\' + file).st_size >size_file :
+        if re.search(r'.+[.](jpg|png|jpeg|gif)', file.lower()) and os.stat(root + '\\' + file).st_size > size_file :
             print(root, file)
             os.replace(root + '\\' + file, '.\\' + directory + '\\'+ file)
-            log_moves[file] = root + '\\' + file
+            log_moves[file] = root + '\\' + file + ';'
             list_movings +=1
 print(f'перемещено {list_movings} файлов изображений')
-print(log_moves)
+# print(log_moves)
 print(size_file)
 input(f'для продолжения нажмите на любую кнопку')
 
