033ba7d076766c280a340f3a006f9b03b5ec6221
rige11 <rigell20@ya.ru>
2022-08-30 22:12:14 +0300

    add support png gif

--- broshurator_0.1.py
+++ broshurator_0.1.py
@@ -4,14 +4,18 @@ import re
 
 directory = '[IMG]'
 
-list_movings = 0
+list_movings = 0  #count number
+log_moves = {}
 if not os.path.isdir('.\\' + directory):
     os.mkdir('.\\' + directory)
 for root, dirs, files in os.walk('.'):
     for file in files:
-        if re.search(r'.+[.]jpe*g', file.lower()) :
+        if re.search(r'.+[.](jpg|png|jpeg|gif)', file.lower()) and os.stat(root + '\\' + file).st_size >150000 :
             print(root, file)
             os.replace(root + '\\' + file, '.\\' + directory + '\\'+ file)
+            log_moves[file] = root + '\\' + file
             list_movings +=1
 print(f'перемещено {list_movings} файлов изображений')
+print(log_moves)
 input(f'для продолжения нажмите на любую кнопку')
+
