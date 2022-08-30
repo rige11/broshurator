5106794dfa64e26e1eaaf34b13a88810df6509c1
rige11 <rigell20@ya.ru>
2022-08-30 22:36:58 +0300

    Update broshurator_0.1.py

--- broshurator_0.1.py
+++ broshurator_0.1.py
@@ -1,8 +1,14 @@
 import os
 import re
+import sys
 
 
 directory = '[IMG]'
+
+if len(sys.argv) > 1:
+    size_file = sys.argv[1]   # read file size from line
+else:
+    size_file = 500000
 
 list_movings = 0  #count number
 log_moves = {}
@@ -10,12 +16,13 @@ if not os.path.isdir('.\\' + directory):
     os.mkdir('.\\' + directory)
 for root, dirs, files in os.walk('.'):
     for file in files:
-        if re.search(r'.+[.](jpg|png|jpeg|gif)', file.lower()) and os.stat(root + '\\' + file).st_size >150000 :
+        if re.search(r'.+[.](jpg|png|jpeg|gif)', file.lower()) and os.stat(root + '\\' + file).st_size >size_file :
             print(root, file)
             os.replace(root + '\\' + file, '.\\' + directory + '\\'+ file)
             log_moves[file] = root + '\\' + file
             list_movings +=1
 print(f'перемещено {list_movings} файлов изображений')
 print(log_moves)
+print(size_file)
 input(f'для продолжения нажмите на любую кнопку')
 
